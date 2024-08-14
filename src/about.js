04b2fa6857a27facd9ee8599d11ed16285d832f3
ser94gio <ser94gio@gmail.com>
2024-08-14 12:48:49 -0400

    improved bio and tab logo

--- src/about.js
+++ src/about.js
@@ -4,9 +4,28 @@ function About() {
   return (
     <div className = "about-body">
       <h1>About Me</h1>
-
+      <p>
+        Hi my name is Sergio DeSousa-Rosa Jr. I am 29 years old and from the State of Rhode Island where I attened
+        the University of Rhode Island. I enjoy spending time outdoors, going to various farmers
+        markets, traveling, and learning about new and interesting fact. <br></br><br></br>
+        I graduated the University 
+        of Rhode Island with a Bachelor of Science in Computer Science in May 2024. I have been working towards the goal
+        of obtaining my bachelors degree for the last 5 years, along with obtaining my Associates in General Studies and 
+        Computer Studies and Information Processing with a focus of computer programming from the Community College of Rhode
+        Island. I am very proud of these accomplishments, beacause:<br></br>
+      </p>
+      <ol className='about-list'>
+        <li>I completed all this before I was 30</li>
+        <li>I beat the odds and obtained a college <br></br>
+        degree even after dropping out of college in 2017
+        </li>
+      </ol>
+      <p>
+        My next goal in life is to find a job within the computer science field hopefully doing some form of developing code and enjoy 
+        my 30's. Hopefully along the way I hope to meet more interesting people and gain further knowledge within the computer science
+        field which I can implement into my own code. Thank you again for visting my site and hope you have a great rest of your day.
+      </p>
     </div>
-    
   );
 }
 
